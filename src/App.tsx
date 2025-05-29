d699e92df2a632ad5fb7a9267717af0f2200e10e
unknown <ritamacharya2004@gmail.com>
2025-05-30 00:06:38 +0530

    add lenis

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css'
 import Main from './components/Main'
+import SmoothScroll from './components/SmoothScroll'
 
 function App() {
   
 
   return (
-    <div className=' bg-black w-full min-h-[100vh] text-white '>
-      <Main />
-      
-    </div>
+    <>
+      <SmoothScroll />
+      <div className=' bg-black w-full min-h-[100vh] text-white '>
+        <Main />
+      </div>
+    </>
   )
 }
 
